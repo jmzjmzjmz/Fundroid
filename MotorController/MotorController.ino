30aa6562ba5faa4bc8463e1fdd20e47e322d1647
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2017-01-08 18:09:48 -0500

    Added parseing for motion commands

--- MotorController/MotorController.ino
+++ MotorController/MotorController.ino
@@ -1,15 +1,49 @@
+// Global Motion Controls
+boolean Armed = false;
+boolean InMotion = false;
+
+
 /*
 The Coordinator port is the port by which the motor communicates to
 the coordinator. The coordinator sends commands here, and the motor 
 controller responds with errors or successes. 
 */
 #define COORDINATOR_PORT Serial1
-enum MotorControls{RotateAbsolute = 0, MoveToPosition = 1, Stop = 2};
+enum MotorControls{RotateAbsolute = 0, MoveToPosition = 1, Stop = 2, Start = 3};
 enum MotorErrors{MotionComplete = 0, RotationFailure = 1, MoveToPositionFailure = 2};
 
-void ParseCommand(char ControlByte, String ControlArgument)
+struct MotionValue
 {
+	MotorControls MotionType;
+	int position;
+};
 
+// Return Success = 0, Failure = 1
+int ParseCommand(char ControlByte, String ControlArgument)
+{
+	MotionValue motion;
+	if(ControlByte == '0')
+	{
+		motion.MotionType = RotateAbsolute;
+		motion.position = ControlArgument.toInt();
+	}
+	else if(ControlByte == '1')
+	{
+		motion.MotionType = MoveToPosition;
+		motion.position = ControlArgument.toInt();
+	}
+	else if(ControlByte == '2')
+	{
+		Armed = false;
+	}
+	else if(ControlByte == '3')
+	{
+		Armed = true;
+	}
+	else
+	{
+		return 1; // Command Not Found
+	}
 }
 
 // Return Success = 0, Failure = 1
@@ -45,6 +79,10 @@ int CheckForCommands()
 			{
 				ControlArgument = COORDINATOR_PORT.readStringUntil('&');
 				state = end;
+			}
+			else
+			{
+				break;
 			}
 		}
 
