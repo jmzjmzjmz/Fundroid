f92570d32ca8f17d02945511c5ca83741d90e890
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2017-01-11 18:01:44 -0500

    Made BNO Read Asynchronous

--- AbsolutePositionerBNO055/AbsolutePositionerBNO055.ino
+++ AbsolutePositionerBNO055/AbsolutePositionerBNO055.ino
@@ -101,39 +101,32 @@ void StartBNO()
   WaitForCalibrationComplete();
 }
 
+long lastBNORead = 0;
+long nextBNORead = BNO055_SAMPLERATE_DELAY_MS;
 void ReadBNO()
 {
-   // Possible vector values can be:
-  // - VECTOR_ACCELEROMETER - m/s^2
-  // - VECTOR_MAGNETOMETER  - uT
-  // - VECTOR_GYROSCOPE     - rad/s
-  // - VECTOR_EULER         - degrees
-  // - VECTOR_LINEARACCEL   - m/s^2
-  // - VECTOR_GRAVITY       - m/s^2
-  imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+  if(millis() - lastBNORead > nextBNORead)
+  {
+    // Possible vector values can be:
+    // - VECTOR_ACCELEROMETER - m/s^2
+    // - VECTOR_MAGNETOMETER  - uT
+    // - VECTOR_GYROSCOPE     - rad/s
+    // - VECTOR_EULER         - degrees
+    // - VECTOR_LINEARACCEL   - m/s^2
+    // - VECTOR_GRAVITY       - m/s^2
+    imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
 
-  /* Display the floating point data */
-  Serial.print("X: ");
-  Serial.print(euler.x());
-  Serial.print(" Y: ");
-  Serial.print(euler.y());
-  Serial.print(" Z: ");
-  Serial.print(euler.z());
-  Serial.print("\t\t");
+    /* Display the floating point data */
+    Serial.print("X: ");
+    Serial.print(euler.x());
+    Serial.print(" Y: ");
+    Serial.print(euler.y());
+    Serial.print(" Z: ");
+    Serial.print(euler.z());
+    Serial.println("\t\t");
 
-  /* Display calibration status for each sensor. */
-  uint8_t system, gyro, accel, mag = 0;
-  bno.getCalibration(&system, &gyro, &accel, &mag);
-  Serial.print("     CALIBRATION: Sys=");
-  Serial.print(system, DEC);
-  Serial.print(" Gyro=");
-  Serial.print(gyro, DEC);
-  Serial.print(" Accel=");
-  Serial.print(accel, DEC);
-  Serial.print(" Mag=");
-  Serial.println(mag, DEC);
-
-  delay(BNO055_SAMPLERATE_DELAY_MS);
+    lastBNORead = millis();
+  }
 }
 
 void setup(void)
